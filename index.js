e354792b4a73673684ea5b3dda029a19c646f33c
Sheridan <sheridanmaustad@gmail.com>
2021-05-04 07:09:15 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const sugarBaby = document.getElementById("text");
+    sugarBaby.textContent = "This is really cool!";
+  });
